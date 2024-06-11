48207bbb85aa37f225cc03929b0a60f00cb6c654
andriypetrulya <50828837+andriypetrulya@users.noreply.github.com>
2024-06-11 12:26:41 +0300

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,22 @@
 
         var visiontextbtn = document.getElementById("visiontextbtn");
 
-        // Добавляем обработчик события клика на кнопку
         visiontextbtn.addEventListener("click", function() {
-            // Создаем новый экземпляр объекта XMLHttpRequest
             var xhr = new XMLHttpRequest();
 
             var visiontextinput = document.querySelector('#visiontextinput').value;
             
-            // Устанавливаем путь к серверному скрипту
             xhr.open("POST", "scripts/visiontext.php", true);
 
-            // Устанавливаем заголовок Content-Type для отправки данных формы
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-            //var userId = document.getElementById('userid').value;
-            // Определяем действие для выполнения на сервере
+        
             var data = "vision=" + visiontextinput;
 
-            // Отправляем запрос на сервер
             xhr.send(data);
 
-            // Обработчик события загрузки данных
             xhr.onload = function() {
                 if (xhr.status == 200) {
-                    // Отображаем результат на странице
                     
                     if(xhr.responseText == 1) {
                         document.getElementById("result").innerHTML = `
@@ -46,34 +38,25 @@
 
 
     
-         // Получаем ссылку на кнопку "Show All Users"
         var visionimagebtn = document.getElementById("visionimagebtn");
 
-        // Добавляем обработчик события клика на кнопку
         visionimagebtn.addEventListener("click", function() {
-            // Создаем новый экземпляр объекта XMLHttpRequest
             var xhr = new XMLHttpRequest();
 
             var visionimageinput = document.querySelector('#visionimageinput').value;
             
-            // Устанавливаем путь к серверному скрипту
-            xhr.open("POST", "scripts/visionimage.php", true);
+=            xhr.open("POST", "scripts/visionimage.php", true);
 
-            // Устанавливаем заголовок Content-Type для отправки данных формы
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-            //var userId = document.getElementById('userid').value;
-            // Определяем действие для выполнения на сервере
+        
             var data = "vision=" + visionimageinput;
 
-            // Отправляем запрос на сервер
             xhr.send(data);
 
-            // Обработчик события загрузки данных
             xhr.onload = function() {
                 if (xhr.status == 200) {
-                    // Отображаем результат на странице
-                    if(xhr.responseText == 1) {
+=                    if(xhr.responseText == 1) {
                         document.getElementById("result").innerHTML = `
                         <img src="images/decorative/not-prohibited-content.png" alt="">
                         <p>Неприпустимого контенту не виявлено!</p>
